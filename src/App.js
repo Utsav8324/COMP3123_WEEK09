92f336e4efd43689953732a7d91862b5b91e12c8
Utsav <127970580+Utsav8324@users.noreply.github.com>
2024-11-03 23:05:58 -0500

    Initial commit for COMP3123 Week 09 lab

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="React logo" />
+        <h1>Welcome to Fullstack Development - I</h1>
+        <h2>React JS Programming Week09 Lab exercise</h2>
+        <p>101413639</p>
+        <p>Utsav Kalathiya</p>
+        <p>George Brown College, Toronto</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
